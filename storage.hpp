e882f401965b9e081ec7948ad8ade999a077beae
atharv ghone <atharvghone@gmail.com>
2026-01-10 22:58:55 -0500

    initial implementation of simple implementation

---
+++ storage.hpp
@@ -0,0 +1,74 @@
+#ifndef STORAGE
+#define STORAGE
+#include <vector>
+
+struct Timestamp {
+  int hour;
+  double min;
+
+  bool operator==(const Timestamp &other_time) const {
+    return hour == other_time.hour && min == other_time.min;
+  }
+
+  bool operator<(const Timestamp &other_time) const {
+    double this_total = (hour * 60) + min;
+    double other_total = (other_time.hour * 60) + other_time.min;
+
+    return this_total < other_total;
+  }
+
+  bool operator>(const Timestamp &other_time) const {
+    return other_time < *this;
+  }
+};
+
+struct Entry {
+  Timestamp time;
+  int value;
+
+  bool operator==(const Entry &other_entry) const {
+    return time == other_entry.time && value == other_entry.value;
+  }
+};
+
+class Storage {
+private:
+  std::vector<Entry> storage;
+  int capacity;
+
+  /*
+   * flush in-memory storage to file
+   */
+  void flush();
+
+public:
+  Storage(int capacity);
+
+  /*
+   * insert entry into storage
+   */
+  void insert(Entry entry);
+
+  /*
+   * get entries before timestamp
+   */
+  std::vector<Entry *> get_before(Timestamp &time);
+
+  /*
+   * get entries after timestamp
+   */
+  std::vector<Entry *> get_after(Timestamp &time);
+
+  /*
+   * get entries in between timestamps
+   */
+  std::vector<Entry *> get_between(Timestamp &before_time,
+                                   Timestamp &after_time);
+
+  /*
+   * delete entry from storage
+   */
+  bool delete_entry(Entry &entry);
+};
+
+#endif // STORAGE
